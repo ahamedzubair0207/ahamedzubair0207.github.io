0eb370940d7df0db5be2bc4e230607dc79f4b321
Kai Ramana <961944@parker.com>
2019-07-22 16:01:37 -0500

    merge fixes

--- src/app/services/mock/mock-menu.ts
+++ src/app/services/mock/mock-menu.ts
@@ -1,8 +1,8 @@
 export const MENU_ITEMS: Array<{ id: string, enabled: boolean, url: string, icon: string, name: string}> = [
     {  id: "favorites", enabled: true, url: "", icon: "fa fa-star", name: 'Favorites' },
-    {  id: "views", enabled: true, url: "views/home", icon: "fa fa-desktop", name: 'Views' },
-    {  id: "organizations", enabled: true, url: "organization/home", icon: "fa fa-sitemap", name: 'Organizations' },
-    {  id: "locations", enabled: true, url: "site/home", icon: "fa fa-location-arrow", name: 'Locations' },
+    {  id: "views", enabled: true, url: "view/home", icon: "fa fa-desktop", name: 'Views' },
+    {  id: "organizations", enabled: true, url: "org/home", icon: "fa fa-sitemap", name: 'Organizations' },
+    {  id: "locations", enabled: true, url: "loc/home", icon: "fa fa-location-arrow", name: 'Locations' },
     {  id: "assets", enabled: true, url: "asset/home", icon: "fa fa-industry", name: 'Assets' },
     {  id: "sensors", enabled: true, url: "sensor/home", icon: "fa fa-thermometer-half", name: 'Sensors' },
     {  id: "gateways", enabled: true, url: "gateway/home", icon: "fa fa-signal", name: 'Gateways' },
