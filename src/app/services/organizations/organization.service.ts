a3a69ae19fd3d3b422d070ed549f571bb6de1093
Kai Ramana <961944@parker.com>
2019-07-29 15:18:04 -0500

    added constants and web config for server

--- src/app/services/organizations/organization.service.ts
+++ src/app/services/organizations/organization.service.ts
@@ -4,13 +4,16 @@ import { CustomHttp } from '../custom_http/custom_http.service';
 import { map } from 'rxjs/operators'
 import { Observable } from 'rxjs';
 import { Organization } from 'src/app/models/organization.model';
+import { AppConstants } from '../../helpers/app.constants';
+
 
 @Injectable({
   providedIn: 'root',
 })
 export class OrganizationService {
 
-  controllerName = 'Organization'
+  // use AppConstants.GET_ORG, CREATE_ORG etc as inputs for the CustomHttp service
+  controllerName = 'Organization';
 
   apiURL: string = '';
 
