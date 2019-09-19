9af40508b3699f63b9e8214310326d67bc617e04
Ahamed Zubair <961952@parker.com>
2019-09-19 09:29:07 -0500

    Added Alert Rule Integration

--- src/app/components/alerts/votm-cloud-alerts-create/votm-cloud-alerts-create.component.ts
+++ src/app/components/alerts/votm-cloud-alerts-create/votm-cloud-alerts-create.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Select2OptionData } from 'ng2-select2';
+import { Alert } from 'src/app/models/alert.model';
 
 @Component({
   selector: 'app-votm-cloud-alerts-create',
@@ -8,9 +9,10 @@ import { Select2OptionData } from 'ng2-select2';
   styleUrls: ['./votm-cloud-alerts-create.component.scss']
 })
 export class VotmCloudAlertsCreateComponent implements OnInit {
+  alert: Alert = new Alert();
 
-  constructor() { 
-   
+  constructor() {
+
   }
 
   ngOnInit() {
