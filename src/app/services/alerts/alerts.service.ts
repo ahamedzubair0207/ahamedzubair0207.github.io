cec22d4ae43fe19950610bfbf1286197b98c6eec
Ahamed Zubair <961952@parker.com>
2019-09-20 00:23:15 -0500

    Added code for Alert RUle UI

--- src/app/services/alerts/alerts.service.ts
+++ src/app/services/alerts/alerts.service.ts
@@ -27,4 +27,11 @@ export class AlertsService {
         map(response => response)
       );
   }
+
+  getAlertRuleSignalAssociatedAssetByOrgId(orgId: string): Observable<any> {
+    return this.http.get(`${AppConstants.GET_ALERT_RULESIGNALS_ASSOCIATION_ASSETS}/${orgId}`)
+      .pipe(
+        map(response => response)
+      );
+  }
 }
