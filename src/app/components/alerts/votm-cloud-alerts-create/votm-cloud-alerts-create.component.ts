54795c3505f44e675b10edf826b081c485e154c0
Ahamed Zubair <961952@parker.com>
2019-10-03 15:31:00 -0500

    Added Updated code from Offshore

--- src/app/components/alerts/votm-cloud-alerts-create/votm-cloud-alerts-create.component.ts
+++ src/app/components/alerts/votm-cloud-alerts-create/votm-cloud-alerts-create.component.ts
@@ -222,6 +222,8 @@ export class VotmCloudAlertsCreateComponent implements OnInit {
   getAbsoluteThreshold() {
     this.absoluteThresholds = [
       {
+
+        // For 3D97A28E-7D8E-4C7D-98CE-251909FED1A9    Absolute ---- Ahamed
         alertConfigurationId: '3A54142B-3453-4232-85C2-EEF4C62E4C77',
         alertConfigurationLabel: 'Low Critical',
         alertConfigurationValue: '',
@@ -237,7 +239,7 @@ export class VotmCloudAlertsCreateComponent implements OnInit {
       },
       {
         alertConfigurationId: '277B236A-C642-461A-A615-175EA69F2FAD',
-        alertConfigurationLabel: 'TargetValue',
+        alertConfigurationLabel: 'Baseline',
         alertConfigurationValue: '',
         class: 'alert-success text-center',
         active: false
@@ -256,6 +258,42 @@ export class VotmCloudAlertsCreateComponent implements OnInit {
         class: 'alert-danger text-center',
         active: false
       },
+      // For B45A2094-C4D6-4D36-B26C-3A9F195C6D6F    Relative --- Ahamed
+      // {
+      //   alertConfigurationId: '3A54142B-3453-4232-85C2-EEF4C62E4C77',
+      //   alertConfigurationLabel: 'Low Critical',
+      //   alertConfigurationValue: '',
+      //   class: 'alert-danger text-center',
+      //   active: false
+      // },
+      // {
+      //   alertConfigurationId: 'C89DBBDF-E927-4044-9A76-F40EF1CE6611',
+      //   alertConfigurationLabel: 'Low Warning',
+      //   alertConfigurationValue: '',
+      //   class: 'alert-warning text-center',
+      //   active: false
+      // },
+      // {
+      //   alertConfigurationId: '277B236A-C642-461A-A615-175EA69F2FAD',
+      //   alertConfigurationLabel: 'Baseline',
+      //   alertConfigurationValue: '',
+      //   class: 'alert-success text-center',
+      //   active: false
+      // },
+      // {
+      //   alertConfigurationId: '4FA3DDCA-56FA-47FA-9251-5D1D7C04C322',
+      //   alertConfigurationLabel: 'High Warning',
+      //   alertConfigurationValue: '',
+      //   class: 'alert-warning text-center',
+      //   active: false
+      // },
+      // {
+      //   alertConfigurationId: '4E045A60-4BEE-44B4-9AF9-151725534706',
+      //   alertConfigurationLabel: 'High Critical',
+      //   alertConfigurationValue: '',
+      //   class: 'alert-danger text-center',
+      //   active: false
+      // },
     ];
   }
 
