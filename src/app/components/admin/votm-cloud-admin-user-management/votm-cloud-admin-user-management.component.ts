181ac5e460212d220de2b9e147d6e515d3417569
Urvisha Seladiya <urvisha.seladiya@EIC.EINFOCHIPS.COM>
2019-10-18 18:35:45 +0530

    Initial Add user modal changes

--- src/app/components/admin/votm-cloud-admin-user-management/votm-cloud-admin-user-management.component.ts
+++ src/app/components/admin/votm-cloud-admin-user-management/votm-cloud-admin-user-management.component.ts
@@ -1,4 +1,9 @@
+import { OrganizationService } from './../../../services/organizations/organization.service';
+import { Organization } from './../../../models/organization.model';
+import { UserService } from './../../../services/users/userService';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { UserRole } from 'src/app/models/user-role';
 
 @Component({
   selector: 'app-votm-cloud-admin-user-management',
@@ -7,9 +12,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VotmCloudAdminUserManagementComponent implements OnInit {
 
-  constructor() { }
+  userForm: FormGroup;
+  roles: UserRole[] = [];
+  organizations: Organization[] = [];
+
+  constructor(
+    private userService: UserService,
+    private organizationService: OrganizationService
+  ) { }
 
   ngOnInit() {
+    this.getRoles();
+    this.getOrganizations();
+  }
+
+  getRoles() {
+    this.userService.getUserRoles().subscribe(
+      response => {
+        this.roles = response;
+      }
+    );
+  }
+
+  getOrganizations() {
+    this.organizationService.getAllOrganizations().subscribe(
+      response => {
+        this.organizations = response;
+      }
+    );
+  }
+
+  onClickOfAddUser() {
+    this.userForm = new FormGroup({
+      firstName: new FormControl(null),
+      lastName: new FormControl(null),
+      emailId: new FormControl(null),
+      roleId: new FormControl(null),
+      phoneNumber: new FormControl(null)
+    });
+  }
+
+  onClickOfSubmitUser() {
+    console.log(this.userForm.value);
   }
 
 }
