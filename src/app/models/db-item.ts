4d931dbf958178c379320ed97d790dcbe77d6515
Ahamed Zubair <961952@parker.com>
2019-10-25 10:15:42 -0500

    Added Code for User Fav

--- src/app/models/db-item.ts
+++ src/app/models/db-item.ts
@@ -1,5 +1,11 @@
 import { Type } from '@angular/core';
 
 export class DbItem {
-  constructor(public id: string, public lName: string, public sName: string, public tplName: string, public component: Type<any>, public widgetConf: any) { }
+  constructor(
+    public id: string, 
+    public lName: string, 
+    public sName: string, 
+    public tplName: string, 
+    public component: Type<any>, 
+    public widgetConf: any) { }
 }
