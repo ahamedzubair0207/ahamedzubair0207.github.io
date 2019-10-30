c85b837165cfd762a2e5ecb5def40d3184d1f6d4
Ahamed Zubair <961952@parker.com>
2019-10-30 09:13:56 -0500

    Added updated code for Favorites

--- src/app/components/sensors/votm-cloud-sensors-home/votm-cloud-sensors-home.component.ts
+++ src/app/components/sensors/votm-cloud-sensors-home/votm-cloud-sensors-home.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+import { VotmCloudConfimDialogComponent } from '../../shared/votm-cloud-confim-dialog/votm-cloud-confim-dialog.component';
+import { ToastrService } from 'ngx-toastr';
+import { Toaster } from '../../shared/votm-cloud-toaster/votm-cloud-toaster';
+import { TreeNode } from 'primeng/api'
+import { SensorsService} from '../../../services/sensors/sensors.service';
 
 @Component({
   selector: 'app-votm-cloud-sensors-home',
@@ -6,44 +12,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./votm-cloud-sensors-home.component.scss']
 })
 export class VotmCloudSensorsHomeComponent implements OnInit {
-  sensorList: any;
-  constructor() { }
+  sensorList: Array<TreeNode>;
+  curLocId: string;
+  curLocName: string;
+  parentOrgId: string;
+  parentOrgName: string;
+  curOrgId: string;
+  curOrgName: string;
+  sensorToDelete: string;
+  message: string;
+
+  @ViewChild('confirmBox', null) confirmBox: VotmCloudConfimDialogComponent;
+  locNameToDelete: any;
+  toaster: Toaster = new Toaster(this.toastr);
+  searchedText: any;
+
+  constructor(private sensorService: SensorsService, 
+    private router: Router, 
+    private route: ActivatedRoute, 
+    private toastr: ToastrService) { }
 
   ngOnInit() {
+    this.route.paramMap.subscribe((params: ParamMap) => {
+      this.getSensorTree();
+    });
+  }
 
-    this.sensorList = [
-      {
-        sensorId: "4820f870-2a48-4564-beb4-b33f4767597e",
-        serialNumber: "001Test",
-        name: "Tempsensoroo112",
-        sensorTypeId: "3daa40a8-4c18-454f-bef2-7d5adc333c8c",
-        modelNumber: null,
-        sensorStatusId: "f68007c9-1413-4ed9-bea8-608462093d75",
-        replacedMappedSensorId: null,
-        active: true,
-        description: "string",
-        createdBy: null,
-        createdOn: null,
-        modifiedBy: null,
-        modifiedOn: null
-      },
-
-      {
-        sensorId: "4820f870-2a48-4564-beb4-b33fwew675972",
-        serialNumber: "002Test",
-        sensorName: "Tempsensoroo113",
-        sensorTypeId: "3daa40a8-4c18-454f-bef2-7d5adc333c5c",
-        modelNumber: null,
-        sensorStatusId: "f68007c9-1413-4ed9-bea8-608462093d25",
-        replacedMappedSensorId: null,
-        active: true,
-        description: "string",
-        createdBy: null,
-        createdOn: null,
-        modifiedBy: null,
-        modifiedOn: null
-      },
-    ];
+  private getSensorTree(){
+    this.sensorService.getSensorTree()
+    .subscribe(response => {
+      this.sensorList = [];
+    });
   }
 
   deleteSensorById(event) {}
--- src/app/services/sensors/sensors.service.ts
+++ src/app/services/sensors/sensors.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { CustomHttp } from '../custom_http/custom_http.service';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { AppConstants } from '../../helpers/app.constants';
+import { HttpParams, HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SensorsService {
 
-  constructor() { }
+  apiURL: string = '';
+
+  constructor(private http: CustomHttp, private httpClient: HttpClient) { }
+
+
+  
+  getSensorTree(){
+    return this.http.get(AppConstants.GET_SENSOR_TREE)
+      .pipe(
+        map(response => response)
+      );
+  }
 }
