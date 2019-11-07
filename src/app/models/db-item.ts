cac5ba10731f7f1a2e5324c609bbc3d006691291
Chirag <chirag.nagar@einfochips.com>
2019-11-07 19:37:37 +0530

    Image overlay Signal detail value

--- src/app/models/db-item.ts
+++ src/app/models/db-item.ts
@@ -2,10 +2,10 @@ import { Type } from '@angular/core';
 
 export class DbItem {
   constructor(
-    public id: string, 
-    public lName: string, 
-    public sName: string, 
-    public tplName: string, 
-    public component: Type<any>, 
+    public id: string,
+    public lName: string,
+    public sName: string,
+    public tplName: string,
+    public component: Type<any>,
     public widgetConf: any) { }
 }
