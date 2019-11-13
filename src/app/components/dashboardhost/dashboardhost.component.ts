1d0ff61983d4821d0ad1be7cf88ace3cb2a97584
Ahamed Zubair <961952@parker.com>
2019-11-13 09:16:31 -0600

    Added code for trend Chart

--- src/app/components/dashboardhost/dashboardhost.component.ts
+++ src/app/components/dashboardhost/dashboardhost.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit, Input, ViewChild, ComponentFactoryResolver } from '@angular/core';
-import { DashboardDirective } from 'src/app/dashboard.directive';
+import { Component, OnInit, Input, ViewChild, ComponentFactoryResolver, SimpleChanges, SimpleChange } from '@angular/core';
 import { DbItem } from 'src/app/models/db-item';
 import { DbTemplate } from 'src/app/models/db-template';
+import { DashboardDirective } from '../dashboards/dashboard.directive';
 
 @Component({
   selector: 'app-dashboardhost',
@@ -13,17 +13,26 @@ export class DashboardhostComponent implements OnInit {
   @Input() dbItem: DbItem;
 
   @ViewChild(DashboardDirective, { static: true }) dashboardHost: DashboardDirective;
-
+componentRef;
   constructor(private cFResolver: ComponentFactoryResolver) { }
 
   ngOnInit() {
     const componentFactory = this.cFResolver.resolveComponentFactory(this.dbItem.component);
-
     const viewContainerRef = this.dashboardHost.viewContainerRef;
     viewContainerRef.clear();
 
-    const componentRef = viewContainerRef.createComponent(componentFactory);
-    (<DbTemplate>componentRef.instance).dbItem = this.dbItem;
+    this.componentRef = viewContainerRef.createComponent(componentFactory);
+    (<DbTemplate>this.componentRef.instance).dbItem = this.dbItem;
+  }
+
+  
+  ngOnChanges(changes: SimpleChanges) {
+    const locked: SimpleChange = changes.locked;
+    // console.log('prev value: ', locked.previousValue);
+    // console.log('got name: ', locked.currentValue);
+    if (this.componentRef) {
+      // (<DbTemplate>this.componentRef.instance).locked = this.locked;
+    }
   }
 
 }
