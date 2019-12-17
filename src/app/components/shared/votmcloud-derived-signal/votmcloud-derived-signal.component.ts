e337412d70b6378ddf4d91d65fe77c449dff84e2
Urvisha Seladiya <urvisha.seladiya@EIC.EINFOCHIPS.COM>
2019-12-17 14:48:32 +0530

    derived signal module added

---
+++ src/app/components/shared/votmcloud-derived-signal/votmcloud-derived-signal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-votmcloud-derived-signal',
+  templateUrl: './votmcloud-derived-signal.component.html',
+  styleUrls: ['./votmcloud-derived-signal.component.scss']
+})
+export class VotmcloudDerivedSignalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
