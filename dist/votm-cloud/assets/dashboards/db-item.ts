bde78588676e680ba3c1d54dc571e58fb0ec242b
Blueooy <axz56040@ucmo.edu>
2020-11-05 19:00:44 -0600

    Merging Conflict

--- dist/votm-cloud/assets/dashboards/db-item.ts
+++
@@ -1,5 +0,0 @@
-import { Type } from '@angular/core';
-
-export class DbItem {
-  constructor(public id: string, public lName: string, public sName: string, public tplName: string, public component: Type<any>, public widgetConf: any) { }
-}
--- dist/votm-cloud/assets/js/data.d.ts
+++
@@ -1,3 +0,0 @@
-// function declaration
-export declare function setData(data);
-export declare function setDashboardConfiguration();
